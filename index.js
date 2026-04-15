cdd83ccd428290fb1bb57390e43b75fec1a099c4
frigotechrs-create <frigotech.rs@gmail.com>
2026-04-16 01:20:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const mqtt = require("mqtt");
 
 console.log("Bridge start...");
 console.log("Waiting for MQTT...");
+
+// KEEP PROCESS ALIVE
+setInterval(() => {
+  console.log("Bridge alive...");
+}, 10000);
